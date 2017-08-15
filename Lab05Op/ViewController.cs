de3fd893d89d246102fb776b0df24e13be6d7a29
Tony Simoes <sr.tony@gmail.com>
2017-08-15 10:42:31 +0200

    Add Buttons

--- Lab05Op/ViewController.cs
+++ Lab05Op/ViewController.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using UIKit;
 
 namespace Lab05Op
@@ -13,7 +13,19 @@ namespace Lab05Op
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+
+            // Después de cargar la vista 
+            // recuperamos los botones en un array
+            // iteramos para añadir los manejadores de evento.
+            UIButton[] Botones = new UIButton[] { Btn_zero, Btn_1, Btn_2, Btn_3, Btn_4, Btn_5, Btn_6, Btn_7, Btn_8, Btn_9 };
+
+            foreach (var item in Botones)
+            {
+                item.TouchUpInside += (object sender, EventArgs e) =>
+                {
+                    LblAffichage.Text = (LblAffichage.Text == "0")? LblAffichage.Text + item.TitleLabel.Text.Substring(1) : LblAffichage.Text + item.TitleLabel.Text;
+                };
+            }
         }
 
         public override void DidReceiveMemoryWarning()
--- Lab05Op/ViewController.designer.cs
+++ Lab05Op/ViewController.designer.cs
@@ -1,17 +1,127 @@
-﻿//
-// This file has been generated automatically to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace Lab05Op
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
-        void ReleaseDesignerOutlets()
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Btn_1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Btn_2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Btn_3 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Btn_4 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Btn_5 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Btn_6 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Btn_7 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Btn_8 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Btn_9 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton Btn_zero { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel LblAffichage { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView ViewPrincipale { get; set; }
+
+        void ReleaseDesignerOutlets ()
         {
+            if (Btn_1 != null) {
+                Btn_1.Dispose ();
+                Btn_1 = null;
+            }
+
+            if (Btn_2 != null) {
+                Btn_2.Dispose ();
+                Btn_2 = null;
+            }
+
+            if (Btn_3 != null) {
+                Btn_3.Dispose ();
+                Btn_3 = null;
+            }
+
+            if (Btn_4 != null) {
+                Btn_4.Dispose ();
+                Btn_4 = null;
+            }
+
+            if (Btn_5 != null) {
+                Btn_5.Dispose ();
+                Btn_5 = null;
+            }
+
+            if (Btn_6 != null) {
+                Btn_6.Dispose ();
+                Btn_6 = null;
+            }
+
+            if (Btn_7 != null) {
+                Btn_7.Dispose ();
+                Btn_7 = null;
+            }
+
+            if (Btn_8 != null) {
+                Btn_8.Dispose ();
+                Btn_8 = null;
+            }
+
+            if (Btn_9 != null) {
+                Btn_9.Dispose ();
+                Btn_9 = null;
+            }
+
+            if (Btn_zero != null) {
+                Btn_zero.Dispose ();
+                Btn_zero = null;
+            }
+
+            if (LblAffichage != null) {
+                LblAffichage.Dispose ();
+                LblAffichage = null;
+            }
+
+            if (ViewPrincipale != null) {
+                ViewPrincipale.Dispose ();
+                ViewPrincipale = null;
+            }
         }
     }
 }
